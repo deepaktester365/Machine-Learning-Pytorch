ffef4d0e8e8474c7905ee0e4a74bd0197bc70ad1
Deepak <deepaktester365@gmail.com>
2022-03-23 17:08:43 -0700

    Pytorch CNN example done

--- neural networks/simple cnn.py
+++ neural networks/simple cnn.py
@@ -7,28 +7,16 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 
-# Set Device
-device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
-
-# Hyperparameters
-input_size = 28
-sequence_length = 28
-num_layers = 2
-hidden_size = 256
-num_classes = 10
-learning_rate = 0.001
-batch_size = 64
-num_epochs = 2
-
-
-# Create a RNN
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size,
-                          num_layers, batch_first=True)
+# Simple CNN
+class CNN(nn.Module):
+    def __init__(self, in_channels=1, num_classes=10):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(
+            3, 3), stride=(1, 1), padding=(1, 1))
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(
+            3, 3), stride=(1, 1), padding=(1, 1))
+        self.fc1 = nn.Linear(16 * 7 * 7, num_classes)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -40,6 +28,15 @@ class RNN(nn.Module):
         return x
 
 
+# Set Device
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+# Hyperparameters
+in_channel = 1
+num_classes = 10
+learning_rate = 0.001
+batch_size = 64
+num_epochs = 5
 
 # Load Data
 train_dataset = datasets.MNIST(
--- neural networks/simple fully connected nn.py
+++ neural networks/simple fully connected nn.py
@@ -7,39 +7,28 @@ import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
 
+# Create fully connected network
+class NN(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(input_size, 50)
+        self.fc2 = nn.Linear(50, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
 # Set Device
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 
 # Hyperparameters
-input_size = 28
-sequence_length = 28
-num_layers = 2
-hidden_size = 256
+input_size = 784
 num_classes = 10
 learning_rate = 0.001
 batch_size = 64
-num_epochs = 2
-
-
-# Create a RNN
-class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(RNN, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size,
-                          num_layers, batch_first=True)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        x = x.reshape(x.shape[0], -1)
-        x = self.fc1(x)
-        return x
-
-
+num_epochs = 1
 
 # Load Data
 train_dataset = datasets.MNIST(
@@ -53,7 +42,7 @@ test_loader = DataLoader(dataset=test_dataset,
                          batch_size=batch_size, shuffle=True)
 
 # Initialize network
-model = CNN().to(device)
+model = NN(input_size=input_size, num_classes=num_classes).to(device)
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -64,6 +53,9 @@ for epoch in range(num_epochs):
     for batch_idx, (data, targets) in enumerate(train_loader):
         data = data.to(device=device)
         targets = targets.to(device=device)
+
+        # Get to correct shape
+        data = data.reshape(data.shape[0], -1)
 
         # forward
         scores = model(data)
@@ -93,6 +85,7 @@ def check_accuracy(loader, model):
         for x, y in loader:
             x = x.to(device=device)
             y = y.to(device=device)
+            x = x.reshape(x.shape[0], -1)
 
             scores = model(x)
             _, predictions = scores.max(1)
